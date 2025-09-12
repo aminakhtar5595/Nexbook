df2f52223d2c794b21cefdee13d6d05307543d15
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-09-12 08:25:02 +0500

    Contacts routes setup

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.use('/api/contacts', require('../Nexbook/routes/contactRoutes'));
+
 app.listen(port, () => {
     console.log('Server is running on port: ', port);
 }) 
