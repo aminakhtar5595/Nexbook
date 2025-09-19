5be05af80d087e093c8571e224b9b1d6299a2736
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-09-19 10:10:19 +0500

    JWT token middleware

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -52,10 +52,10 @@ const loginUser = asyncHandler(async (req, res) => {
 
 const currentUser = asyncHandler(async (req, res) => {
     try {
+        res.status(200).json(req.user);
     } catch (error) {
         console.log('Error: ', error);
     }
 });
-
 
 module.exports = { registerUser, loginUser, currentUser };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { registerUser, loginUser, currentUser } = require('../controllers/userControllers');
+const validateToken = require('../middleware/validateTokenHandler');
 const router = express.Router();
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
-router.route('/current').get(currentUser);
+router.route('/current').get(validateToken, currentUser);
 
 module.exports = router;
